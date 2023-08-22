624b5547d64db115a9a7423af988b22752e01512
Hugo Fargues <hugo.fargues@gmail.com>
2023-08-22 11:11:52 +0200

    feat: dark mode for banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import styles from './Banner.module.css';
 import { useTranslations } from 'next-intl';
+import { fr } from '@codegouvfr/react-dsfr';
+import { useColors } from '@codegouvfr/react-dsfr/useColors';
 
 function Banner() {
+  const theme = useColors();
   const t = useTranslations('Banner');
 
   return (
-    <div className={styles.bandeau}>
+    <div
+      className={styles.banner}
+      style={{
+        color: fr.colors.getHex({ isDark: !theme.isDark }).options.grey._50_1000.default,
+        backgroundColor: fr.colors.getHex({ isDark: !theme.isDark }).options.blueFrance._925_125.default,
+      }}
+    >
       <span className={styles.mobileTitle}>{t('mobileTitle')}</span>
       <span className={styles.webTitle}>{t('webTitle')}</span>
     </div>
