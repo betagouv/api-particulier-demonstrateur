151320fac78d65fb919439f40aaca234a0d6d03d
Hugo Fargues <hugo.fargues@gmail.com>
2023-08-31 19:05:44 +0200

    feat: revert setJourney on index page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,9 +6,16 @@ import { Tag } from '@codegouvfr/react-dsfr/Tag';
 import Tooltip from '@/components/Tooltip';
 import { useTranslations } from 'next-intl';
 import Banner from '@/components/Banner';
+import { useJourney } from '@/app/journey-provider';
+import { Journey } from '@/app/types';
 
 export default function Index() {
   const t = useTranslations('UsageAndUser');
+  const { setJourney } = useJourney();
+
+  const handleCardClick = (data: Journey) => {
+    setJourney(data);
+  };
 
   return (
     <div className={common.container}>
@@ -17,39 +24,62 @@ export default function Index() {
         <h1 className={`fr-h2 ${common.title}`}>{t('title')}</h1>
         <h2 className={`fr-h1 ${common.subtitle}`}>{t('titleUsage')} </h2>
         <div className={common.cards}>
-          <Card
-            className={common.card}
-            start={
-              <ul className="fr-tags-group">
-                <li>
-                  <Tag small>{t('tags.socialPricing')}</Tag>
-                </li>
-              </ul>
+          <div
+            role="button"
+            onClick={() =>
+              handleCardClick({
+                name: null,
+                type: 'transport',
+                description: null,
+                isFranceConnectAuth: null,
+              })
             }
-            desc={t('cards.transport.desc')}
-            enlargeLink
-            linkProps={{
-              href: '/choix-personnage?usage=1',
-            }}
-            title={t('cards.transport.title')}
-          />
-
-          <Card
-            className={common.card}
-            start={
-              <ul className="fr-tags-group">
-                <li>
-                  <Tag small>{t('tags.solidarityPricing')}</Tag>
-                </li>
-              </ul>
+          >
+            <Card
+              className={common.card}
+              start={
+                <ul className="fr-tags-group">
+                  <li>
+                    <Tag small>{t('tags.socialPricing')}</Tag>
+                  </li>
+                </ul>
+              }
+              desc={t('cards.transport.desc')}
+              enlargeLink
+              linkProps={{
+                href: '/choix-personnage?usage=1',
+              }}
+              title={t('cards.transport.title')}
+            />
+          </div>
+          <div
+            role="button"
+            onClick={() =>
+              handleCardClick({
+                name: null,
+                type: 'canteen',
+                description: null,
+                isFranceConnectAuth: null,
+              })
             }
-            desc={t('cards.canteen.desc')}
-            enlargeLink
-            linkProps={{
-              href: '/choix-personnage?usage=2',
-            }}
-            title={t('cards.canteen.title')}
-          />
+          >
+            <Card
+              className={common.card}
+              start={
+                <ul className="fr-tags-group">
+                  <li>
+                    <Tag small>{t('tags.solidarityPricing')}</Tag>
+                  </li>
+                </ul>
+              }
+              desc={t('cards.canteen.desc')}
+              enlargeLink
+              linkProps={{
+                href: '/choix-personnage?usage=2',
+              }}
+              title={t('cards.canteen.title')}
+            />
+          </div>
         </div>
       </main>
       <Tooltip disabledActions={{ back: true, home: true }} isOpenedByDefault={false}></Tooltip>
